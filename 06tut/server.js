0fe99fa01af7852222c9eb16acfab055d9789512
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2022-07-02 22:28:21 +0900

    Done 06tut

---
+++ 06tut/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.ridirect(301, '/new-page.html');
+});
+
+// Route Handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('html.html をロード下');
+    next();
+}, (req, res) => {
+    res.send('next() でハローワールド');
+});
+
+const one = (req, res, next) => {
+    console.log('one');
+    next()
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next()
+}
+const three = (req, res, next) => {
+    console.log('three');
+    res.send('おしまし');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(PORT, () => console.log(`サーバーがポート番号 ${PORT} で起動しています`));
